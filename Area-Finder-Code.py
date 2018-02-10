e42174f3b7c1c2eb373f6707bbec1b2ffd068b7c
peaches6 <skpikao233@gmail.com>
2018-02-10 17:38:52 +0000

    Move area finder into function called infinitely and add superscript

--- Area-Finder-Code.py
+++ Area-Finder-Code.py
@@ -1,4 +1,11 @@
-height = float(input("Please enter the height: "))
-width  = float(input("Please enter the width: "))
-area = height * width
-print("The area in metres squared is", area)
+def AreaFinder():
+    height = float(input("Please enter the height: "))
+
+    width  = float(input("Please enter the width: "))
+
+    area = height * width
+
+    print("The area is {}cm\u00b2".format(area), "\n")
+
+while(True):
+    AreaFinder()
