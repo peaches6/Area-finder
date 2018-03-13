2153c213315caf1e2a1bc46c2b202c0692f9fb12
Mike_Went <git@meew.me>
2018-03-13 20:43:34 +0300

    Migrate to Python 3; add Ctrl+C handling

--- Area-Finder-Code.py
+++ Area-Finder-Code.py
@@ -1,11 +1,12 @@
-def AreaFinder():
-    height = float(input("Please enter the height: "))
+#!/usr/bin/env python3
+def area_find(height, width):
+    return float(height) * float(width)
 
-    width  = float(input("Please enter the width: "))
-
-    area = height * width
-
-    print("The area is {}cm\u00b2".format(area), "\n")
-
-while(True):
-    AreaFinder()
+while True:
+    try:
+        height = input("Please enter the height: ")
+        width  = input("Please enter the width: ")
+        area = area_find(height, width)
+        print("The area is {} cm²".format(area), "\n")
+    except KeyboardInterrupt:
+        exit()
